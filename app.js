ab66dcaf7e3172bc4c00e59cba538b1448fececd
mark <markpackham1@gmail.com>
2023-12-07 15:48:57 +0000

    add title and description to homepage
    https://youtu.be/PAm_QcN6Ffs?feature=shared&t=945

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 
 const app = express();
+// http://localhost:5000/
 const port = 5000 || process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
@@ -17,3 +18,17 @@ app.use(express.static("public"));
 app.use(expressLayouts);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
+
+// Home
+app.get("/", (req, res) => {
+  const locals = {
+    title: "NodeJs",
+    description: "Free NodeJs User Management System",
+  };
+
+  res.render("index", { locals });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
