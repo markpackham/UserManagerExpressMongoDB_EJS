00386465a4d6534cb76aec96aaecffbfbc1e7776
mark <markpackham1@gmail.com>
2023-12-13 17:30:12 +0000

    fix bugs in database connection
    https://youtu.be/PAm_QcN6Ffs?si=AfKcXgM9bAQ4c4do&t=2640

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
   }
