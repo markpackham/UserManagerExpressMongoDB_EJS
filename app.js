f2aaa8c86ababc60cfe812b1b17cc1ac01f002dc
mark <markpackham1@gmail.com>
2023-12-13 15:53:11 +0000

    add 404 page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const res = require("express/lib/response");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -19,14 +20,18 @@ app.use(expressLayouts);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
-// Home
-app.get("/", (req, res) => {
-  const locals = {
-    title: "NodeJs",
-    description: "Free NodeJs User Management System",
-  };
+// // Home
+// app.get("/", (req, res) => {
+//   const locals = {
+//     title: "NodeJs",
+//     description: "Free NodeJs User Management System",
+//   };
+//   res.render("index", { locals });
+// });
 
-  res.render("index", { locals });
+// 404 Page
+app.get("*", (req, res) => {
+  res.status(404).render("404");
 });
 
 app.listen(port, () => {
